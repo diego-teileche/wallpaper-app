ab65339a9bfd4604877bf2c60a829e2012d42781
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-03 17:35:21 -0300

    Home Screen | Input
    - Input text created
    - Icons added
    - Styles added

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,7 +1,14 @@
-import { Pressable, StyleSheet, Text, View } from "react-native"
-import React from "react"
+import {
+	Pressable,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	View,
+} from "react-native"
+import React, { useRef, useState } from "react"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
-import { FontAwesome6 } from "@expo/vector-icons"
+import { Feather, FontAwesome6, Ionicons } from "@expo/vector-icons"
 import { theme } from "@/constants/theme"
 import { StatusBar } from "expo-status-bar"
 import { hp, wp } from "@/helpers/common"
@@ -9,6 +16,9 @@ import { hp, wp } from "@/helpers/common"
 const HomeScreen = () => {
 	const { top } = useSafeAreaInsets()
 	const paddingTop = top > 0 ? top + 10 : 30
+
+	const [search, setSearch] = useState("")
+	const searchInputRef = useRef(null)
 
 	return (
 		<View style={[styles.container, { paddingTop }]}>
@@ -27,6 +37,36 @@ const HomeScreen = () => {
 					/>
 				</Pressable>
 			</View>
+
+			<ScrollView contentContainerStyle={{ gap: 15 }}>
+				<View style={styles.searchBar}>
+					<View style={styles.searchIcon}>
+						<Feather
+							name="search"
+							size={24}
+							color={theme.colors.neutral(0.4)}
+						/>
+					</View>
+
+					<TextInput
+						placeholder="Search for photos..."
+						value={search}
+						ref={searchInputRef}
+						onChangeText={(value) => setSearch(value)}
+						style={styles.searchInput}
+					/>
+
+					{search && (
+						<Pressable style={styles.closeIcon}>
+							<Ionicons
+								name="close"
+								size={24}
+								color={theme.colors.neutral(0.6)}
+							/>
+						</Pressable>
+					)}
+				</View>
+			</ScrollView>
 		</View>
 	)
 }
@@ -46,6 +86,32 @@ const styles = StyleSheet.create({
 		fontSize: hp(4),
 		fontWeight: theme.fontWeights.semibold,
 		color: theme.colors.neutral(0.9),
+	},
+	searchBar: {
+		marginHorizontal: wp(4),
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		borderWidth: 1,
+		borderColor: theme.colors.grayBG,
+		backgroundColor: theme.colors.neutral(0.1),
+		padding: 6,
+		paddingLeft: 10,
+		borderRadius: theme.radius.lg,
+	},
+	searchIcon: {
+		padding: 8,
+	},
+	searchInput: {
+		flex: 1,
+		borderRadius: theme.radius.sm,
+		paddingVertical: 10,
+		fontSize: hp(1.8),
+	},
+	closeIcon: {
+		backgroundColor: theme.colors.neutral(0.2),
+		padding: 8,
+		borderRadius: theme.radius.sm,
 	},
 })
 
