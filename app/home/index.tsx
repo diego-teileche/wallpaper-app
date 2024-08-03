f2a81141987fb59c00dcf203a36a5cd6c2a76f1c
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-02 21:09:13 -0300

    Home Screen
    - Safe area added
    - Header with title and icon added
    - Styles added

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,14 +1,52 @@
-import { StyleSheet, Text, View } from "react-native"
+import { Pressable, StyleSheet, Text, View } from "react-native"
 import React from "react"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+import { FontAwesome6 } from "@expo/vector-icons"
+import { theme } from "@/constants/theme"
+import { StatusBar } from "expo-status-bar"
+import { hp, wp } from "@/helpers/common"
 
 const HomeScreen = () => {
+	const { top } = useSafeAreaInsets()
+	const paddingTop = top > 0 ? top + 10 : 30
+
 	return (
-		<View>
-			<Text>HomeScreen</Text>
+		<View style={[styles.container, { paddingTop }]}>
+			<StatusBar style="dark" />
+
+			<View style={styles.header}>
+				<Pressable>
+					<Text style={styles.title}>Pixels</Text>
+				</Pressable>
+
+				<Pressable>
+					<FontAwesome6
+						name="bars-staggered"
+						size={22}
+						color={theme.colors.neutral(0.7)}
+					/>
+				</Pressable>
+			</View>
 		</View>
 	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		gap: 15,
+	},
+	header: {
+		marginHorizontal: wp(4),
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	title: {
+		fontSize: hp(4),
+		fontWeight: theme.fontWeights.semibold,
+		color: theme.colors.neutral(0.9),
+	},
+})
 
 export default HomeScreen
